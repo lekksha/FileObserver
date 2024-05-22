8400d2a2a1f6d72d2a42737f48da63f178281591
lekksha_ <lekksha@gmail.com>
2024-05-22 14:44:15 +0700

    make output compact and adaptable

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
 
-    QString path = "C:/Users/lekks/TSU/semester 6/TRPO/5-12/lab3/testFiles";
+    QString path = "C:/Users/lekks/TSU/semester 6/TRPO/5-12/lab3/testFiles/";
     unsigned int flag = CalculationStrategy::EXTENSION;
     execute(path, flag);
 
